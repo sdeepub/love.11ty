c4ea4ebeae264f33194eb03b3b170b5ad9334a63
Odd Lenovo Sway <odd.daboss@gmail.com>
2026-08-25 22:40:55 +0700

    fix: implement html base plugin to resolve asset subfolder paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-
+const { HtmlBasePlugin } = require("@11ty/eleventy");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const embedYouTube = require("eleventy-plugin-youtube-embed");
 
@@ -14,7 +14,9 @@ module.exports = function(config) {
   config.addFilter("squash", require("./src/utils/filters/squash.js") );
   config.addFilter("dateDisplay", require("./src/utils/filters/date.js") );
 
-
+  // HtmlBase
+  config.addPlugin(HtmlBasePlugin);
+  
   // add support for syntax highlighting
   config.addPlugin(syntaxHighlight);
 
