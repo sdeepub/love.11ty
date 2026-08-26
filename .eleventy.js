3359d438aab79e42c2ae199aef33e18bf57c0362
Odd Lenovo Sway <odd.daboss@gmail.com>
2026-08-26 09:43:40 +0700

    fix: adjust background image to relative pathing template

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,8 @@ const embedYouTube = require("eleventy-plugin-youtube-embed");
 
 module.exports = function(config) {
 
-  // A useful way to reference the context we are runing eleventy in
-  let env = process.env.ELEVENTY_ENV;
+  // A useful way to reference the context we are running eleventy in
+  let env = process.env.ELEVENTY_ENV || "dev";
 
   // Layout aliases can make templates more portable
   config.addLayoutAlias('default', 'layouts/base.njk');
@@ -14,7 +14,7 @@ module.exports = function(config) {
   config.addFilter("squash", require("./src/utils/filters/squash.js") );
   config.addFilter("dateDisplay", require("./src/utils/filters/date.js") );
 
-  // HtmlBase
+  // HtmlBase Plugin to automatically handle subfolder path-prefixes
   config.addPlugin(HtmlBasePlugin);
   
   // add support for syntax highlighting
@@ -37,19 +37,21 @@ module.exports = function(config) {
       return minified.code;
   });
 
-
   // pass some assets right through
   config.addPassthroughCopy("./src/site/images");
 
-  //custom css
+  // FIX: Explicitly pass through BOTH stylesheets into your output directory
+  config.addPassthroughCopy({ "./src/site/_includes/css/main.css": "css/main.css" });
   config.addPassthroughCopy({ "./src/site/_includes/css/custom.css": "css/custom.css" });
 
   // make the seed target act like prod
-  env = (env=="seed") ? "prod" : env;
+  env = (env == "seed") ? "prod" : env;
+
   return {
     dir: {
       input: "src/site",
       output: "dist",
+      // FIX: Standardize pathing string format relative to the input folder root
       data: `_data/${env}`
     },
     templateFormats : ["njk", "md", "11ty.js"],
